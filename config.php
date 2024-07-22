c16e378a85c07474b2795170c7fc2537edd72087
brunodlj <bruno.2022310667@aluno.iffar.edu.br>
2024-07-22 17:07:22 -0300

    login
    inicio do login

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$congig = [
+    'host'=> 'localhost',
+    'user'=> 'root',
+    'pass'=> '',
+    'db' => 'trab-manter',
+    'email' => 'bruno.2022310667@aluno.iffar.edu.br',
+    'senha_email' => 'ggbtwfapsuwfttee'
+];
